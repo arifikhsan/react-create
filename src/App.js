45db0f2d31572814dbba32f6ff97a1c445a77888
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-21 09:21:10 +0700

    20 add and delete state

--- src/App.js
+++ src/App.js
@@ -11,15 +11,28 @@ class App extends Component {
     ],
   };
 
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({ ninjas });
+  };
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter((ninja) => {
+      return ninja.id !== id;
+    });
+    this.setState({ ninjas });
+  };
+
   render() {
     const { ninjas } = this.state;
 
     return (
       <div className="App">
         <h1>My first react app</h1>
-        <p>Welcome</p>
-        <Ninjas ninjas={ninjas} />
-        <AddNinja />
+        <p>Welcome :)</p>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={ninjas} />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
