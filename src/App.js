e6f2ca2398251a5fc9b847c60884592a58454f17
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-21 07:58:11 +0700

    deconstructor

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Ninjas from "./Ninjas";
 class App extends Component {
   state = {
     ninjas: [
-      {name: 'Ryu', age: 30, belt: 'black', id: 1},
-      {name: 'Yoshi', age: 12, belt: 'green', id: 2},
-      {name: 'Crystal', age: 32, belt: 'pink', id: 3},
-    ]
-  }
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 12, belt: "green", id: 2 },
+      { name: "Crystal", age: 32, belt: "pink", id: 3 },
+    ],
+  };
+  
   render() {
+    const { ninjas } = this.state;
+
     return (
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas ninjas={ninjas} />
       </div>
     );
   }
